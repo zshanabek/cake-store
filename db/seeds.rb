19427de0a455bb49c962bfa54afd130ed801e40c
Zhunissali <zshanabek@gmail.com>
2017-06-27 22:12:05 +0600

    [Add] - Added sign_in out buttons and admin privilleges

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create! name: 'Jonas', lastname:"Shanabek", phone:87785547554, gender: "Male", age: 42, password:"451524aa", admin:false, email:"zhunissali.shanabek@ce.sdu.edu.kz"
+User.create! name: 'Elmira', lastname:"Dzhanibekova", phone:87016781045, gender: "Male", age: "52", password:"451524aa", admin:true, email:"zhunissali.shanabek@outlook.com"
